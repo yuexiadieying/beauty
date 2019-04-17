ffa19c7d94ef0e9b94bfa52d450e0c5861ede1e3
cjk18 <cus18@sina.com>
2019-04-17 14:29:29 +0800

    delete branchtest

---
+++ beauty-project/traffic/src/main/java/com/meixiang/beauty/webapp/traffic/route/RouteController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.meixiang.beauty.webapp.traffic.route;
+
+import com.meixiang.beauty.common.web.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 工具 Controller
+ *
+ * @author ThinkGem
+ * @version 2013-10-17
+ */
+@Controller
+public class RouteController extends BaseController {
+
+    @RequestMapping(value ="",method = {RequestMethod.POST, RequestMethod.GET})
+    public String traffic(HttpServletResponse response) {
+        response.addHeader("Pragma","no-cache");
+        response.setHeader("Cache-Control","no-cache");
+        response.setHeader("Expires","0");
+        return "trafficIndex";
+    }
+
+
+}
+
