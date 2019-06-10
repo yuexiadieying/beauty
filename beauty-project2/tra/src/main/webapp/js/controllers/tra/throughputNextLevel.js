a72b5976620b37cc49111fd0dbb6da5cc4825052
去 <qq>
2019-06-10 15:47:35 +0800

    港口企业和码头单位名录、全省港区代码表、全省港口表

--- beauty-project2/tra/src/main/webapp/js/controllers/tra/throughputNextLevel.js
+++ beauty-project2/tra/src/main/webapp/js/controllers/tra/throughputNextLevel.js
@@ -5,15 +5,15 @@
 app.controller('ThroughputNextLevelCtrl', ['$scope','$stateParams', function($scope,$stateParams) {
 
     $scope.folds = [
-        {name: '全省泊位表', filter:''},
-        {name: '港口企业和码头单位名录', filter:'starred'},
-        {name: '企业操作人员', filter:'sent'},
-        {name: '全省港区代码表', filter:'important'},
-        {name: '全省港口表', filter:'draft'},
-        {name: '港口快报吞吐量', filter:'express'},
-        {name: '港口吞吐量', filter:'a'},
-        {name: '行业管理机构', filter:'b'},
-        {name: '行业管理人员', filter:'c'}
+        {name: '全省泊位表', filter:'',url:'list'},
+        {name: '港口企业和码头单位名录', filter:'starred',url:'port'},
+        {name: '企业操作人员', filter:'sent',url:'management'},
+        {name: '全省港区代码表', filter:'important',url:'port'},
+        {name: '全省港口表', filter:'draft',url:'port'},
+        {name: '港口快报吞吐量', filter:'express',url:'list'},
+        {name: '港口吞吐量', filter:'a',url:'list'},
+        {name: '行业管理机构', filter:'b',url:'management'},
+        {name: '行业管理人员', filter:'c',url:'management'}
     ];
     $scope.fold = $stateParams.fold;
 
@@ -27,3 +27,68 @@ app.controller('ThroughputNextLevelListCtrl', ['$scope', '$stateParams', functio
     $scope.tableTitle= [['表年份','归属港区编号','泊位代码','泊位名称','服务类型','货主类型','泊位长度','设计靠泊能力(吨级)','增加的报告期','报废的报告期','泊位状态','码头企业号'],['泊位编号','报表报告','报表行号','合计','外贸','出港数据','进港数据']]
 
 }]);
+app.controller('ThroughputNextLevelPortCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+    $scope.fold = $stateParams.fold;
+    $scope.tableTitle= [
+        ['唯一标识号','报表年份','辖机构编码','经济类型','联系电话','报表填报人(联系人)','创建报告期','注销时的报告期','企业状态'],
+        ['序号','全国统一港区编号','港区名称','所在水域','所在水系','港区管理单位'],
+        ['序号','全国统一港口编号','港口名称','港口归属机构编码','报告期类型','港口管理单位']
+    ]
+
+}]);
+app.controller('ThroughputNextLevelManagementCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+    $scope.fold = $stateParams.fold;
+    $scope.tableTitle= [
+        ['人员编号','所在企业编号','操作者名','密码','创建时间','帐号状态','最近修改密码时间','联系电话','操作权限','申请口令校验码','注销日期'],
+        ['机构编码','机构全称','机构所在行政区划名称','机构负责人姓名','部门负责人','填报人姓名','联系电话'],
+        ['人员编码','用户所在机构编码','操作者名','操作口令','创建时间','帐号状态','最近修改密码时间','联系电话','操作权限','申请口令校验码的MD5','注销日期']
+    ]
+
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
