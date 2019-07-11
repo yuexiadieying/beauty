00b935f696ae83d311efe4fbd4700e537e8946d5
root <12345678>
2019-07-11 12:52:42 +0800

    chenjiake

--- beauty-project2/tra/src/main/webapp/js/controllers/tra/userList.js
+++ beauty-project2/tra/src/main/webapp/js/controllers/tra/userList.js
@@ -2,27 +2,44 @@
 
 /* Controllers */
 
-app.controller('UserListCtrl', ['$scope', function($scope) {
+app.controller('UserListCtrl', ['$scope', '$http','Global','traUtil',function($scope,$http,Global,traUtil) {
+
+    traUtil.getUserInfo();
 
     $scope.param = {
-       data:[
-           {
-               firstetterL:'x',
-               name:['小李','小丽','晓丽']
-           },
-           {
-               firstetterL:'y',
-               name:['雨琪','于琪','玉祁']
-           },
-           {
-               firstetterL:'z',
-               name:['张云','张芸','张赟','张云娜','张允']
-           },
-       ]
+        data:[],
+        dataList:[]
+    }
 
+    $http.get('/traffic/user/getAllUserList', {})
+        .then(function(response) {
+            if(response.data.result==Global.SUCCESS) {
+                console.log(response.data.responseData);
+                angular.forEach(response.data.responseData,function (val,index) {
+                    var flag = false;
+                    var dataIndex = 0;
+                    angular.forEach($scope.param.dataList,function (val1,index1) {
+                        if(val.department.id == val1.firstetterL)
+                        {
+                            flag = true;
+                            dataIndex = index1;
+                        }
+                    })
+                    if(flag)
+                    {
+                        $scope.param.dataList[dataIndex].userInfoList.push(angular.copy(val));
+                    }else {
+                        $scope.param.dataList.push({'firstetterL':angular.copy(val.department.departmentName),'userInfoList':[angular.copy(val)]})
+                    }
 
-
-   }
+                })
+                console.log($scope.param.dataList);
+            }else {
+                alert(response.data.errorInfo);
+            }
+        }, function(x) {
+            $scope.authError = 'Server Error';
+        });
 
 }]);
 
