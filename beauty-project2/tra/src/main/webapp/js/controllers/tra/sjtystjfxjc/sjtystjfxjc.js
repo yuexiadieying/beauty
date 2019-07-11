16ae50f1b6fac994c54a41f757c5c85f710edfa0
root <12345678>
2019-07-11 13:07:50 +0800

    chenjiake

--- beauty-project2/tra/src/main/webapp/js/controllers/tra/sjtystjfxjc/sjtystjfxjc.js
+++ beauty-project2/tra/src/main/webapp/js/controllers/tra/sjtystjfxjc/sjtystjfxjc.js
@@ -4,7 +4,33 @@
 
 app
   // Flot Chart controller 
-  .controller('SjtystjfxjcCtrl', ['$scope', function($scope) {
+  .controller('SjtystjfxjcCtrl', ['$scope','$stateParams','traUtil', '$http','Global','$state',
+      function($scope,$stateParams,traUtil,$http,Global,$state) {
+
+      traUtil.getUserInfo();
+
+      $http.get('/traffic/sso/encryptSSO')
+          .then(function(response) {
+              console.log(response.data);
+              if (response.data.result==Global.SUCCESS) {
+                  $scope.param.url = response.data.responseData.platformURL
+                      +"?platformFlag="+response.data.responseData.platformFlag+'&&'
+                      +'secretStr='+response.data.responseData.platformEncrypt;
+
+              }else{
+                  $scope.thirdPlatformError = response.data.errorInfo;
+              }
+          }, function(x) {
+              $scope.authError = 'Server Error';
+          });
+
+      $scope.goThirdPlatform = function () {
+          if($scope.param.url!=undefined){
+              window.location.href = $scope.param.url;
+          }else{
+              alert($scope.thirdPlatformError);
+          }
+      }
 
       $scope.d3 = [
           { label: "指标一", data: 40 },
