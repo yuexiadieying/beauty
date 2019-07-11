da781a336334d00a6a349a5f8365995e84fce885
root <12345678>
2019-07-11 12:51:11 +0800

    chenjiake

--- beauty-project2/tra/src/main/webapp/js/controllers/tra/zhgj.js
+++ beauty-project2/tra/src/main/webapp/js/controllers/tra/zhgj.js
@@ -37,17 +37,25 @@ app
 
       $http.get('/traffic/sso/encryptSSO')
           .then(function(response) {
+              console.log(response.data);
               if (response.data.result==Global.SUCCESS) {
-                  console.log(response.data.responseData);
                   $scope.param.url = response.data.responseData.platformURL
                       +"?platformFlag="+response.data.responseData.platformFlag+'&&'
                       +'secretStr='+response.data.responseData.platformEncrypt;
 
               }else{
-                  alert(response.data.errorinfo);
+                  $scope.thirdPlatformError = response.data.errorInfo;
               }
           }, function(x) {
               $scope.authError = 'Server Error';
           });
+
+      $scope.goThirdPlatform = function () {
+          if($scope.param.url!=undefined){
+              window.location.href = $scope.param.url;
+          }else{
+              alert($scope.thirdPlatformError);
+          }
+      }
 
   }]);
